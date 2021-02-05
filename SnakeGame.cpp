5497e1d2c418871e418f1c0fe8f40d732dcb5657
Pythonlover2020 <64594972+Pythonlover2020@users.noreply.github.com>
2021-02-05 21:49:00 +0800

    Update SnakeGame.cpp

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -1,126 +1,137 @@
-/*coding:WINDOWS-936*/
 #include "SnakeGame.h"
 
-SnakeGame::SnakeGame():SDLWindow(SDL_INIT_VIDEO,"\xe8\xb4\xaa\xe5\x90\x83\xe8\x9b\x87",WIDTH,HEIGHT,SDL_WINDOW_SHOWN)
+SnakeGame::SnakeGame():SDLWindow(SDL_INIT_VIDEO,"\xe8\xb4\xaa\xe5\x90\x83\xe8\x9b\x87",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,WIDTH,HEIGHT,SDL_WINDOW_SHOWN)
 {
+    imageSurface = NULL;
+    imageTexture = NULL;
     gameState = START;
-    this->image = nullptr;
+    fp = fopen("SnakeGame.log","w");
+    if (!fp)
+    {
+        perror("fopen");
+        exit(1);
+    }
 
-    this->snake = new Snake;
-    init_snake();
-    init_food();
-}
-
-void SnakeGame::init_snake()
-{
-    this->snake->size = 2;
-    this->snake->body[0].x = WIDTH / 2 - 5;
-    this->snake->body[0].y = HEIGHT / 2 - 5;
-
-    this->snake->body[1].x = snake->body[0].x;
-    this->snake->body[1].y = snake->body[0].y + 10;
+    snake = new Snake;
+    food = new FOOD;
 }
 
 SnakeGame::~SnakeGame()
 {
-    delete snake;
-    if (imageTexture != nullptr)
+    if (fp != NULL)
+    {
+        fclose(fp);
+    }
+
+    if (imageSurface != NULL)
+    {
+        SDL_FreeSurface(imageSurface);
+        imageSurface = NULL;
+    }
+
+    if (imageTexture != NULL)
     {
         SDL_DestroyTexture(imageTexture);
-        imageTexture = nullptr;
+        imageTexture = NULL;
     }
 }
 
-void SnakeGame::start()
-{
-    this->eventLoop();
-}
-
-void SnakeGame::draw(int dx,int dy)
+void SnakeGame::draw()
 {
     SDL_SetRenderDrawColor(renderer,0x00,0x00,0x00,0xFF);
     SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawBlendMode(renderer,SDL_BLENDMODE_BLEND);
-
-    switch (this->gameState)
+    switch (gameState)
     {
     case START:
-        if (imageTexture != nullptr)
+        imageSurface = SDL_LoadBMP("start_game.bmp");
+        if (!imageSurface)
         {
-            SDL_DestroyTexture(imageTexture);
-            imageTexture = nullptr;
-        }
-        this->image = SDL_LoadBMP("start_game.bmp");
-        if (!image)
-        {
-            SDL_Log("Cannot open start_game.bmp!");
+            fprintf(fp,"Cannot open start_game.bmp!\n");
             exit(1);
         }
-        imageTexture = SDL_CreateTextureFromSurface(renderer,image);
-        SDL_FreeSurface(this->image);
-        this->image = nullptr;
+        imageTexture = SDL_CreateTextureFromSurface(renderer,imageSurface);
+        SDL_FreeSurface(imageSurface);
+        imageSurface = NULL;
         SDL_RenderCopy(renderer,imageTexture,NULL,NULL);
         break;
-    case LOSE:
-        if (imageTexture != nullptr)
-        {
-            SDL_DestroyTexture(imageTexture);
-            imageTexture = nullptr;
-        }
-        this->image = SDL_LoadBMP("lose.bmp");
-        if (!image)
-        {
-            SDL_Log("Cannot open lose.bmp!");
-            exit(1);
-        }
-        imageTexture = SDL_CreateTextureFromSurface(renderer,image);
-        SDL_FreeSurface(this->image);
-        this->image = nullptr;
-        SDL_RenderCopy(renderer,imageTexture,NULL,NULL);
-        break;
-    case WIN:
-        if (imageTexture != nullptr)
-        {
-            SDL_DestroyTexture(imageTexture);
-            imageTexture = nullptr;
-        }
-        this->image = SDL_LoadBMP("win.bmp");
-        if (!image)
-        {
-            SDL_Log("Cannot open win.bmp!");
-            exit(1);
-        }
-        imageTexture = SDL_CreateTextureFromSurface(renderer,image);
-        SDL_FreeSurface(this->image);
-        this->image = nullptr;
-        SDL_RenderCopy(renderer,imageTexture,NULL,NULL);
-        break;
-    case PLAYING:
-        if (this->snake->size == (WIDTH * HEIGHT) / 100)
-        {
-            gameState = WIN;
-        }
-        for (int i = this->snake->size - 1; i > 0; --i)
-        {
-            this->snake->body[i].x = this->snake->body[i - 1].x;
-            this->snake->body[i].y = this->snake->body[i - 1].y;
-        }
-        this->snake->body[0].x += dx;
-        this->snake->body[0].y += dy;
 
-        // Éß²»ÄÜ×²Ç½£¬²»ÄÜ×²×Ô¼º
-        if (this->snake->body[0].x <= 0 || this->snake->body[0].x >= WIDTH || this->snake->body[0].y <= 0 || this->snake->body[0].y >= HEIGHT)
+    case WIN:
+        if (imageTexture != NULL)
+        {
+            SDL_DestroyTexture(imageTexture);
+            imageTexture = NULL;
+        }
+        imageSurface = SDL_LoadBMP("win.bmp");
+        if (!imageSurface)
+        {
+            fprintf(fp,"Cannot open win.bmp!\n");
+            exit(1);
+        }
+        imageTexture = SDL_CreateTextureFromSurface(renderer,imageSurface);
+        SDL_FreeSurface(imageSurface);
+        imageSurface = NULL;
+        SDL_RenderCopy(renderer,imageTexture,NULL,NULL);
+        break;
+
+    case LOSE:
+        if (imageTexture != NULL)
+        {
+            SDL_DestroyTexture(imageTexture);
+            imageTexture = NULL;
+        }
+        imageSurface = SDL_LoadBMP("lose.bmp");
+        if (!imageSurface)
+        {
+            fprintf(fp,"Cannot open lose.bmp!\n");
+            exit(1);
+        }
+        imageTexture = SDL_CreateTextureFromSurface(renderer,imageSurface);
+        SDL_FreeSurface(imageSurface);
+        imageSurface = NULL;
+        SDL_RenderCopy(renderer,imageTexture,NULL,NULL);
+        break;
+
+    case PLAYING:
+        update();
+
+        if (snake->body[0].x <= 0 || snake->body[0].x >= WIDTH || snake->body[0].y <= 0 || snake->body[0].y >= HEIGHT)
         {
             gameState = LOSE;
-            break;
         }
 
-        for (int i = 1; i < this->snake->size; ++i)
+        for (int i = 1; i < snake->size; ++i)
         {
-            if (this->snake->body[0].x == this->snake->body[i].x && this->snake->body[0].y == this->snake->body[i].y)
+            if (snake->body[0].x == snake->body[i].x && snake->body[0].y == snake->body[i].y)
             {
                 gameState = LOSE;
+                break;
+            }
+        }
+
+        if (snake->body[0].x == food->x && snake->body[0].y == food->y)
+        {
+            ++snake->size;
+            initFood();
+            if (dy == 10)
+            {
+                snake->body[snake->size - 1].x = snake->body[snake->size - 2].x;
+                snake->body[snake->size - 1].y = snake->body[snake->size - 2].y - 10;
+            }
+            else if (dy == -10)
+            {
+                snake->body[snake->size - 1].x = snake->body[snake->size - 2].x;
+                snake->body[snake->size - 1].y = snake->body[snake->size - 2].y + 10;
+            }
+            else if (dx == 10)
+            {
+                snake->body[snake->size - 1].x = snake->body[snake->size - 2].x - 10;
+                snake->body[snake->size - 1].y = snake->body[snake->size - 2].y;
+            }
+            else if (dx == -10)
+            {
+                snake->body[snake->size - 1].x = snake->body[snake->size - 2].x + 10;
+                snake->body[snake->size - 1].y = snake->body[snake->size - 2].y;
             }
         }
 
@@ -129,173 +140,122 @@ void SnakeGame::draw(int dx,int dy)
             break;
         }
 
-        // ÊÇ·ñ×²µ½Ê³Îï
-        if (this->snake->body[0].x >= food.x && this->snake->body[0].x <= food.x + 10 && this->snake->body[0].y >= food.y && this->snake->body[0].y <= food.y + 10)
-        {
-            ++this->snake->size;
-            if (dx == 10)
-            {
-                this->snake->body[this->snake->size - 1].x = this->snake->body[this->snake->size - 2].x - 10;
-                this->snake->body[this->snake->size - 1].y = this->snake->body[this->snake->size - 2].y;
-            }
-            else if (dx == -10)
-            {
-                this->snake->body[this->snake->size - 1].x = this->snake->body[this->snake->size - 2].x + 10;
-                this->snake->body[this->snake->size - 1].y = this->snake->body[this->snake->size - 2].y;
-            }
-            else if (dy == 10)
-            {
-                this->snake->body[this->snake->size -1].x = this->snake->body[this->snake->size -2].x;
-                this->snake->body[this->snake->size - 1].y = this->snake->body[this->snake->size - 2].y - 10;
-            }
-            else if (dy == -10)
-            {
-                this->snake->body[this->snake->size -1].x = this->snake->body[this->snake->size -2].x;
-                this->snake->body[this->snake->size - 1].y = this->snake->body[this->snake->size - 2].y + 10;
-            }
-
-            init_food();
-        }
-
-        SDL_SetRenderDrawColor(renderer,0x00,0xFF,0xFF,0xFF);
-        SDL_Rect rect = {this->snake->body[0].x,this->snake->body[0].y,10,10};
-        SDL_RenderFillRect(renderer,&rect);
+        SDL_Rect headrect = {snake->body[0].x,snake->body[0].y,10,10};
+        SDL_SetRenderDrawColor(renderer, 0x00,0xFF,0xFF,0xFF);
+        SDL_RenderFillRect(renderer,&headrect);
         for (int i = 1; i < snake->size; ++i)
         {
-            SDL_SetRenderDrawColor(renderer,0xFF,0xFF,0xFF,0xFF);
-            SDL_Rect rect = {this->snake->body[i].x,this->snake->body[i].y,10,10};
+            SDL_Rect rect = {snake->body[i].x,snake->body[i].y,10,10};
+            SDL_SetRenderDrawColor(renderer, 0xFF,0xFF,0xFF,0xFF);
             SDL_RenderFillRect(renderer,&rect);
         }
-
-        SDL_Rect foodrect = {food.x,food.y,10,10};
+        SDL_Rect foodRect = {food->x,food->y, 10,10};
         SDL_SetRenderDrawColor(renderer,0x00,0xFF,0x00,0xFF);
-        SDL_RenderFillRect(renderer,&foodrect);
+        SDL_RenderFillRect(renderer,&foodRect);
+        break;
     }
 
     SDL_RenderPresent(renderer);
 }
 
-void SnakeGame::eventLoop()
+void SnakeGame::start()
 {
-    static int dx = 0;
-    static int dy = -10;
     while (true)
     {
-        int begin = SDL_GetTicks();
         SDL_Event event;
+
+        int begin = SDL_GetTicks();
 
         while (SDL_PollEvent(&event))
         {
             switch (event.type)
             {
             case SDL_QUIT:
-                goto END;
+                return;
+
+            case SDL_MOUSEBUTTONDOWN:
+                if (gameState == START)
+                {
+                    dx = 0;
+                    dy = -10;
+                    initSnake();
+                    initFood();
+                    gameState = PLAYING;
+                }
+                break;
+
             case SDL_KEYDOWN:
                 switch (event.key.keysym.sym)
                 {
-                case SDLK_s:
-                    init_snake();
-                    init_food();
-                    this->gameState = PLAYING;
-                    break;
                 case SDLK_ESCAPE:
-                    goto END;
-
-                    // ÉßÒÆ¶¯
+                    return;
+                case SDLK_s:
+                    dx = 0;
+                    dy = -10;
+                    initSnake();
+                    initFood();
+                    gameState = PLAYING;
+                    break;
                 case SDLK_LEFT:
-                    switch (gameState)
-                    {
-                    case PLAYING:
-                        dx = -10;
-                        dy = 0;
-                        break;
-                    case START:
-                        break;
-                    case WIN:
-                        break;
-                    case LOSE:
-                        break;
-                    }
+                    dx = -10;
+                    dy = 0;
                     break;
 
                 case SDLK_RIGHT:
-                    switch (gameState)
-                    {
-                    case PLAYING:
-                        dx = 10;
-                        dy = 0;
-                        break;
-                    case START:
-                        break;
-                    case WIN:
-                        break;
-                    case LOSE:
-                        break;
-                    }
+                    dx = 10;
+                    dy = 0;
                     break;
 
                 case SDLK_UP:
-                    switch (gameState)
-                    {
-                    case PLAYING:
-                        dx = 0;
-                        dy = -10;
-                        break;
-                    case START:
-                        break;
-                    case WIN:
-                        break;
-                    case LOSE:
-                        break;
-                    }
+                    dx = 0;
+                    dy = -10;
                     break;
-
                 case SDLK_DOWN:
-                    switch (gameState)
-                    {
-                    case PLAYING:
-                        dx = 0;
-                        dy = 10;
-                        break;
-                    case START:
-                        break;
-                    case WIN:
-                        break;
-                    case LOSE:
-                        break;
-                    }
+                    dx = 0;
+                    dy = 10;
                     break;
                 }
-                break;
-            case SDL_MOUSEBUTTONDOWN:
-                switch (this->gameState)
-                {
-                case START:
-                    this->gameState = PLAYING;
-                    break;
-                }
-                break;
             }
         }
 
-        this->draw(dx,dy);
+        draw();
 
         int end = SDL_GetTicks();
         int time_ = end - begin;
-        int delay = (1000 / FRAMESTATE) - time_;
+        int rate = 1000 / FRAMERATE;
+        int delay = rate - time_;
 
-        if (delay > 0 && gameState == PLAYING)
+        if (delay > 0)
         {
             SDL_Delay(delay);
         }
     }
-END:
-    return;
 }
 
-void SnakeGame::init_food()
+void SnakeGame::initFood()
 {
-    srand((unsigned)time(NULL) + rand());
-    food.x = (rand() % (WIDTH / 10)) * 10;
-    food.y = (rand() % (HEIGHT / 10)) * 10;
+    srand((unsigned)time(NULL) + rand()); // æ›´æ–°éšæœºæ•°ç§å­
+    food->x = (rand() % (WIDTH / 10)) * 10;
+    food->y = (rand() % (HEIGHT / 10)) * 10;
+}
+
+void SnakeGame::initSnake()
+{
+    snake->size = 2; // ä¸€ä¸ªè›‡å¤´ï¼Œä¸€ä¸ªè›‡èº«
+    snake->body[0].x = WIDTH / 2;
+    snake->body[0].y  = HEIGHT / 2;
+    snake->body[1].x = snake->body[0].x;
+    snake->body[1].y = snake->body[0].y + 10;
+}
+
+void SnakeGame::update()
+{
+    for (int i = snake->size; i > 0; --i)
+    {
+        snake->body[i].x = snake->body[i - 1].x;
+        snake->body[i].y = snake->body[i - 1].y;
+    }
+    // æ›´æ–°è›‡å¤´
+    snake->body[0].x += dx;
+    snake->body[0].y += dy;
 }
